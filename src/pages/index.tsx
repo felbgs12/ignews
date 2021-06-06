121701cd141398654326aa2f288fd33f698b5304
Felipe Borges <felbgs12@gmail.com>
2021-06-06 16:31:26 -0300

    feat: pass priceId prop to the SubscribeButton component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import styles from './home.module.scss';
 
 interface HomeProps {
   product: {
-    productId: string;
+    priceId: string;
     amount: number;
   };
 }
@@ -30,7 +30,7 @@ export default function Home({ product }: HomeProps) {
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
