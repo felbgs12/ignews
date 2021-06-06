5c62344adf7b7a2b31824315c7e1ff8fe6c15bbe
Felipe Borges <felbgs12@gmail.com>
2021-06-06 16:28:02 -0300

    feat: adds stripe and consumes api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    productId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,7 +28,7 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
           <SubscribeButton />
         </section>
@@ -29,3 +38,23 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Iz0QjJRKpTfzqAqbu8A82zz', {
+    expand: ['product'],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100), // Divided by 100 because price are in cents
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
