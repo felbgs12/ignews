e3314db0b89df1aeaf68ff05b62f577d2fd030d4
Felipe Borges <felbgs12@gmail.com>
2021-06-06 17:15:50 -0300

    feat: adds mocked api users

---
+++ src/pages/api/users.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    { id: 1, name: 'Felipe' },
+    { id: 2, name: 'John' },
+    { id: 3, name: 'Doe' },
+  ];
+
+  return response.json(users);
+};
