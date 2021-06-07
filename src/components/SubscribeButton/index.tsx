5e8be9f291ff8cff565a972ac83a57f4daed61aa
Felipe Borges <felbgs12@gmail.com>
2021-06-07 13:18:05 -0300

    feat: create subscribe api route

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -5,6 +5,8 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  
+
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
