27160601b137d82b7aaa2501cb40ed977f77882e
Felipe Borges <felbgs12@gmail.com>
2021-06-07 11:02:42 -0300

    feat: saves user to DB when login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
+import { signIn } from 'next-auth/client';
 import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,20 @@ export default NextAuth({
       scope: 'read:user', // Defines the scope of the information that you can see of the user
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
