35cb8708d39bed1c564aa210a09358fccce907fd
Felipe Borges <felbgs12@gmail.com>
2021-06-20 13:24:46 -0300

    fix: stripe construct event

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,10 +5,9 @@ import { stripe } from '../../../services/stripe';
 
 export async function saveSubscription(
   subscriptionId: string,
-  customerId: string
+  customerId: string,
+  createAction = false
 ) {
-  // Search user user at FaunaDB by the ID {customerId}
-  // Save the subscription Data in FaunaDB
   console.log(subscriptionId, customerId);
   const userRef = await fauna.query(
     q.Select(
@@ -26,7 +25,19 @@ export async function saveSubscription(
     price_id: subscription.items.data[0].price.id,
   };
 
-  await fauna.query(
-    q.Create(q.Collection('subscriptions'), { data: subscriptionData })
-  );
+  if (createAction) {
+    await fauna.query(
+      q.Create(q.Collection('subscriptions'), { data: subscriptionData })
+    );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          'ref',
+          q.Get(q.Match(q.Index('subscription_by_id'), subscriptionId))
+        ),
+        { data: subscriptionData }
+      )
+    );
+  }
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -10,6 +10,8 @@ async function buffer(readable: Readable) {
   for await (const chunk of readable) {
     chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
   }
+
+  return Buffer.concat(chunks);
 }
 
 export const config = {
@@ -18,7 +20,11 @@ export const config = {
   },
 };
 
-const relevantEvents = new Set(['checkout.session.completed']);
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -43,13 +49,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       console.log('Evento recebido ', event);
       try {
         switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
           case 'checkout.session.completed':
             const checkoutSession = event.data
               .object as Stripe.Checkout.Session;
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
             break;
           default:
