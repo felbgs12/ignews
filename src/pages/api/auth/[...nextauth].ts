e8bbddde46368c90e70436b075519066e7d072a8
Felipe Borges <felbgs12@gmail.com>
2021-06-21 21:27:49 -0300

    feat: try catch if found the user, but the subscription isn't active

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -18,32 +18,37 @@ export default NextAuth({
   },
   callbacks: {
     async session(session) {
-      session.user.email;
-
-      const userActiveSubscription = await fauna.query(
-        q.Get(
-          q.Intersection([
-            q.Match(
-              q.Index('subscription_by_user_ref'),
-              q.Select(
-                'ref',
-                q.Get(
-                  q.Match(
-                    q.Index('user_by_email'),
-                    q.Casefold(session.user.email)
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
                   )
                 )
-              )
-            ),
-            q.Match(q.Index('subscription_by_statys'), 'active'),
-          ])
-        )
-      );
+              ),
+              q.Match(q.Index('subscription_by_statys'), 'active'),
+            ])
+          )
+        );
 
-      return {
-        ...session,
-        activeSubscription: userActiveSubscription,
-      };
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
     },
     async signIn(user, account, profile) {
       const { email } = user;
